66a76c42cac238540647529e785f661ed84e56f0
Jared Hester <jared.hester@gmail.com>
2014-11-15 19:14:26 -0500

    added cat project

---
+++ src/Cats/CatTagger.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading;
+using EditorUtils;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Tagging;
+
+namespace Cats
+{
+    /// <summary>
+    /// Tag all occurences of "cat" in the code base
+    /// </summary>
+    internal sealed class CatTagger : AsyncTaggerSource<string, TextMarkerTag>
+    {
+        #region BackgroundWorker
+
+        sealed class BackgroundWorker
+        {
+            private readonly string _word = "cat";
+            private readonly CancellationToken _cancellationToken;
+
+            internal BackgroundWorker(CancellationToken cancellationToken)
+            {
+                _cancellationToken = cancellationToken;
+            }
+
+            internal ReadOnlyCollection<ITagSpan<TextMarkerTag>> GetTags(SnapshotSpan span)
+            {
+                var tags = new List<ITagSpan<TextMarkerTag>>();
+                var lineRange = SnapshotLineRange.CreateForSpan(span);
+                Debug.WriteLine("Cat Version {0}, Lines {1} - {2}", span.Snapshot.Version.VersionNumber, lineRange.StartLineNumber, lineRange.LastLineNumber);
+                foreach (var snapshotLine in lineRange.Lines)
+                {
+                    AddWordsOnLine(tags, snapshotLine);
+                    _cancellationToken.ThrowIfCancellationRequested();
+                }
+
+                // Cats need naps
+                Debug.WriteLine("Cat Nap Time");
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                Debug.WriteLine("Cat Wake Up");
+
+                return tags.ToReadOnlyCollectionShallow();
+            }
+
+            private void AddWordsOnLine(List<ITagSpan<TextMarkerTag>> tags, ITextSnapshotLine snapshotLine)
+            {
+                var snapshot = snapshotLine.Snapshot;
+                var tag = new TextMarkerTag(Constants.FormatName);
+
+                var i = 0;
+                while (i < snapshotLine.Length)
+                {
+                    var point = snapshot.GetPoint(i + snapshotLine.Start.Position);
+                    if (IsWord(point))
+                    {
+                        var span = new SnapshotSpan(snapshot, snapshotLine.Start.Position + i, _word.Length);
+                        tags.Add(new TagSpan<TextMarkerTag>(span, tag));
+                        i += _word.Length;
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                }
+            }
+
+            private bool IsWord(SnapshotPoint point)
+            {
+                var snapshot = point.Snapshot;
+                int i;
+                for (i = 0; i + point.Position < snapshot.Length && i < _word.Length; i++)
+                {
+                    if (snapshot.GetChar(i + point.Position) != _word[i])
+                    {
+                        return false;
+                    }
+                }
+
+                if (i < snapshot.Length && Char.IsLetter(snapshot.GetChar(i + point.Position)))
+                {
+                    return false;
+                }
+
+                return true;
+            }
+        }
+
+        #endregion
+
+        internal CatTagger(ITextView textView)
+            : base(textView)
+        {
+
+        }
+
+        #region AsyncTaggerSource
+
+        protected override bool TryGetTagsPrompt(SnapshotSpan span, out IEnumerable<ITagSpan<TextMarkerTag>> tags)
+        {
+            tags = null;
+            return false;
+        }
+
+        protected override string GetDataForSnapshot(ITextSnapshot snapshot)
+        {
+            return string.Empty;
+        }
+
+        protected override ReadOnlyCollection<ITagSpan<TextMarkerTag>> GetTagsInBackground(string data, SnapshotSpan span, CancellationToken cancellationToken)
+        {
+            var backgroundWorker = new BackgroundWorker(cancellationToken);
+            return backgroundWorker.GetTags(span);
+        }
+
+        #endregion
+    }
+}
---
+++ src/Cats/CatTaggerProvider.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.Composition;
+using EditorUtils;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Utilities;
+
+namespace Cats
+{
+    [Export(typeof(IViewTaggerProvider))]
+    [ContentType("any")]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    [TagType(typeof(TextMarkerTag))]
+    internal sealed class CatTaggerProvider : IViewTaggerProvider
+    {
+        private readonly object _key = new object();
+
+        [ImportingConstructor]
+        internal CatTaggerProvider()
+        {
+#if DEBUG
+            EditorUtilsTrace.TraceSwitch.Level = System.Diagnostics.TraceLevel.Info;
+#endif
+        }
+
+        #region IViewTaggerProvider
+
+        ITagger<T> IViewTaggerProvider.CreateTagger<T>(ITextView textView, ITextBuffer textBuffer)
+        {
+            if (textView.TextBuffer != textBuffer)
+            {
+                return null;
+            }
+
+            var tagger = EditorUtilsFactory.CreateTagger<string, TextMarkerTag>(
+                textView.Properties,
+                _key,
+                () => new CatTagger(textView));
+
+            return (ITagger<T>)(object)tagger;
+        }
+
+        #endregion
+    }
+}
