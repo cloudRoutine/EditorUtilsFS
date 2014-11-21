b381e8b2075e0822f4e1fa210cea1296eea8b5ce
Jared Hester <jared.hester@gmail.com>
2014-11-21 01:30:49 -0500

    Implemented IBasicUndoHistoryRegistry

---
+++ src/EditorUtilsFS/IBasicUndoHistoryRegistry.fs
@@ -0,0 +1,38 @@
+﻿namespace EditorUtilsFS
+
+open Microsoft.VisualStudio.Text.Operations;
+
+
+    type IBasicUndoHistory =
+        inherit ITextUndoHistory
+        /// <summary>
+        /// Clear out all of the state including the undo and redo stacks
+        /// </summary>
+        abstract Clear : unit
+
+    /// <summary>
+    /// In certain hosted scenarios the default ITextUndoHistoryRegistry won't be 
+    /// available.  This is a necessary part of editor composition though and some 
+    /// implementation needs to be provided.  Importing this type will provide a 
+    /// very basic implementation
+    ///
+    /// This type intentionally doesn't ever export ITextUndoHistoryRegistry.  Doing
+    /// this would conflict with Visual Studios export and cause a MEF composition 
+    /// error.  It's instead exposed via this interface 
+    ///
+    /// In general this type won't be used except in testing
+    /// </summary>
+    type IBasicUndoHistoryRegistry =
+        /// <summary>
+        /// Get the basic implementation of the ITextUndoHistoryRegistry
+        /// </summary>
+        abstract TextUndoHistoryRegistry : ITextUndoHistoryRegistry with get
+
+        /// <summary>
+        /// Try and get the IBasicUndoHistory for the given context
+        /// </summary>
+        // TODO - in editor utils this function takes a IBasicUndoHistory ref as an out, not sure if that
+        // strategy should be kept or if returning a tuple is fine
+        abstract TryGetBasicUndoHistory : context:obj -> basicUndoHistory:IBasicUndoHistory -> bool * IBasicUndoHistory 
+
+
