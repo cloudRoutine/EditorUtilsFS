6b8088b4c074545288c93c69cb618ecfd0e14f79
Jared Hester <jared.hester@gmail.com>
2014-11-21 01:00:56 -0500

    Implemented EditorUtilsTrace

---
+++ src/EditorUtilsFS/Core/EditorUtilsTrace.fs
@@ -0,0 +1,20 @@
+﻿namespace EditorUtilsFS
+open System
+open System.Diagnostics
+
+type EditorUtilsTrace() =
+    static let traceSwitch = TraceSwitch("EditorUtilsFS", "EditorUtilsFS Trace")
+    
+    static do 
+        traceSwitch.Level = TraceLevel.Off |> ignore
+
+    static member TraceSwitch with get() = traceSwitch
+
+    [<Conditional("TRACE")>]
+    static member TraceInfo (message:string) =
+        Trace.WriteLineIf(traceSwitch.TraceInfo, "EditorUtilsFs: " + message)
+
+
+    [<Conditional("TRACE")>]
+    static member TraceInfo (message:string, [<ParamArray>] args: obj[]) =
+        Trace.WriteLineIf( traceSwitch.TraceInfo, "EditorUtilsFs: " + String.Format(message,args))
